757ac757358f1134afd2f109e4c20a2e18079c90
ChaosO9 <irfannoorh@gmail.com>
2023-05-12 11:00:39 +0800

    Small Changes

--- absensi/login.php
+++ absensi/login.php
@@ -1,16 +1,22 @@
 <?php
     session_start();
+
+    //Mengecek apakah user sudah login sebelumnya
     if (isset($_SESSION["login"]) ) {
         header("Location: index.php");
     }
+
+    //Menghubungkan database
     require 'koneksi.php';
+
+    //Proses login dan pengecekan password
     if( isset($_POST["login"]) ) {
         $email = $_POST["email"];
         $password = $_POST["password"];
 
        $result = mysqli_query($koneksi, "SELECT * FROM dosen WHERE email ='$email'");
 
-       if( mysqli_num_rows($result) === 1 ) {
+    if( mysqli_num_rows($result) === 1 ) {
             while($row = mysqli_fetch_assoc($result)) {
                 if( password_verify($password, $row["password"]) ) {
                     $_SESSION["login"] = true;
@@ -24,57 +30,56 @@
                     exit;
                 }
             }
-       }
-       
-       $error = true;
+    }
+    //Flag error bahwa password/email salah
+    $error = true;
     }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Login Admin</title>
+    <title>Login Admin | Sistem Absensi TI 4A</title>
 </head>
+
 <body class="login">
     <div class="container">
         <div class="row justify-content-center form-login mt-5">
             <div class="col-md-6">
                 <form action="" class="panel" method="post">
-                    <h3 class="mb-4 text-center text-uppercase">Login </h3>
+                    <h3 class="mb-4 text-center text-uppercase">Login Admin Absen TI 4A</h3>
                     <?php if( isset($error) ) :?>
                     <div class="alert alert-danger mr-5 ml-5 radius" role="alert">
-                    Email / Password salah
+                        Email / Password Anda salah
                     </div>
                     <?php endif; ?>
                     <div class="form-group ml-5 mr-5">
-                        <input type="text" name="email" id="email" class="form-control form-control-lg radius" placeholder="Email">
+                        <input type="text" name="email" id="email" class="form-control form-control-lg radius"
+                            placeholder="Email">
                     </div>
                     <div class="form-group ml-5 mr-5">
-                        <input type="password" name="password" id="password" class="form-control form-control-lg radius" placeholder="Password">
+                        <input type="password" name="password" id="password" class="form-control form-control-lg radius"
+                            placeholder="Password">
                     </div>
                     <div class="form-group mt-4 ml-5 mr-5">
-                    <button type="submit" class="btn btn-info btn-login block radius" name="login">Login</button>
+                        <button type="submit" class="btn btn-info btn-login block radius" name="login">Login</button>
                     </div>
                     <!-- <div class="form-group mt-4 ml-5 mr-5">
                     <a href="registrasidosen.php" class="btn btn-info btn-regis block radius" role="button">Registrasi</a>
                     </div> -->
-
                 </form>
-
             </div>
         </div>
     </div>
-
-
-
-
-    <script src="js/bootstrap.min.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
